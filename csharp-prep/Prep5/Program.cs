27f77d29edc41ab93743a55b8516c01f3b16bc9a
jschaefer18 <joshschaefer@gmail.com>
2024-05-01 11:45:50 -0600

    finished prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,42 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("What is your favorite number: ");
+        string userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
+        return number;
+    }
+
+    static int SquareNumber(int num)
+    {
+        return num * num;
+    }
+
+    static void DisplayResult(int sqrt,string name)
+    {
+        Console.WriteLine($"Hi {name}, your favorite number squared is {sqrt}.");
+    }
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int sqrt = SquareNumber(number);
+        DisplayResult(sqrt,name);
+
+
     }
 }
